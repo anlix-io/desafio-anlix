1c5b29d2ae32cabfcea14a6b158d87a93a29a207
Quelvinmp <quelvinmp@gmail.com>
2023-06-14 09:22:06 -0300

    feat: generic file reading functions

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,38 @@
 const express = require('express');
-const { getPatientsByName } = require('./utils/fsUtils');
+const { getPatientsByName, getPatientsByNameAndDisease } = require('./utils/fsUtils');
 
 const app = express();
 
-app.get('/patient/:name', (req, res) => {
+const validateName = (req, res, next) => {
   const result = getPatientsByName(req.params.name)
 
-  if (result.length) return res.status(200).json({result})
-  res.status(404).send({message: 'Patient Not Found'});
+  if (!result.length) {
+    return res.status(404).send({message: 'Patient Not Found'});
+  }
+  return next();
+}
+
+const validateDisease = (req, res, next) => {
+  const disease = req.params.disease.toLowerCase();
+
+  if (disease === 'cardiaco' || disease === 'pulmonar') {
+    return next();
+  }
+  return res.status(404).send({message: 'Disease Not Found'});
+}
+
+app.get('/patient/:name', validateName, (req, res) => {
+  const result = getPatientsByName(req.params.name)
+
+return res.status(200).json({result})
 });
+
+app.get('/patient/:name/:disease', validateName, validateDisease, (req, res) => {
+  const { name, disease } = req.params;
+
+  const result = getPatientsByNameAndDisease(name.toLowerCase(), disease.toLowerCase())
+
+})
 
 
 module.exports = app;
--- backend/src/utils/fsUtils.js
+++ backend/src/utils/fsUtils.js
@@ -1,11 +1,52 @@
-const fs = require('fs');
+const fs = require('fs').promises;
+const path = require('path');
 
 const patients = require('../../dados/pacientes.json')
-const ind_car_dir = 'backend/dados/indice_cardiaco';
-const ind_pul_dir = 'backend/dados/indice_pulmonar';
+const ind_car_dir = path.resolve(__dirname, '../../dados/indice_cardiaco');
+const ind_pul_dir = path.resolve(__dirname, '../../dados/indice_pulmonar');
+
+const readDir = async (dir) => {
+  try {
+    const files = await fs.readdir(dir);
+    return files
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const readArchivesAndSeparateByLine = async (archives, chosenDisease) => {
+  try {
+    informationFound = [];
+
+    await Promise.all(archives.map(async (archive) => {
+      const completePath = path.join(chosenDisease, archive);
+      
+      const data = await fs.readFile(completePath, 'utf8');
+
+      const lines = data.split('\n');
+
+      lines.forEach((line) => {
+          const [cpf, epoch, ind] = line.split(' ');
+
+          if (cpf !== 'CPF') informationFound.push({cpf, epoch, ind})
+      })
+    }))
+    return informationFound;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 const getPatientsByName = (query) => patients.filter((patient) => patient.nome.toLowerCase().includes(query.toLowerCase()));
 
+const getPatientsByNameAndDisease = async (name, disease) => {
+  const chosenDisease = disease === 'cardiaco' ? ind_car_dir : ind_pul_dir;
+  const archives = await readDir(chosenDisease);
+  const result = await readArchivesAndSeparateByLine(archives, chosenDisease);
+  console.log(result);
+}
+
 module.exports = {
-  getPatientsByName
+  getPatientsByName,
+  getPatientsByNameAndDisease
 }
